b6debe30bf699219ed55eae3344343121b98514b
Matteo-Grellier <matteogrellier@gmail.com>
2025-01-30 14:41:11 +0100

    feat(css): add pandacss
    handle cors error

--- back/go.mod
+++ back/go.mod
@@ -15,6 +15,7 @@ require (
 )
 
 require (
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { css } from '../styled-system/css';
+import { flex } from '../styled-system/patterns';
+import { button } from '../recipes/button'
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [test, settest] = useState();
+
+	const printlog = () => {
+		fetch('http://localhost:8080/temp')
+			.then(response => response.json())
+			.then(data => settest(data.temp))
+	}
+
+	const socket = new WebSocket("ws://localhost:8080")
+
+	socket.addEventListener("open", () => { socket.send("connection established") })
+
+	socket.addEventListener("message", event => {
+		console.log("Message from server ", event.data)
+	});
+
+	useEffect(() => {
+		console.log(test)
+	}, [test])
+
 
 	return (
-		<>
-			<div></div>
-		</>
+		<div
+			className={flex({
+				justify: 'center',
+				align: 'center',
+				width: '100vw',
+				height: '100vh',
+				direction: 'column',
+			})}
+		>
+			<div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>
+				Hello 🐼!
+			</div>
+			
+			<button className={button({ visual: 'solid', size: 'lg' })} onClick={printlog}>
+				Click Me
+			</button>
+			<div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>
+				{test}
+			</div>
+		</div>
 	)
 }
 
