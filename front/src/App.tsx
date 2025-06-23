72ced82b11ab1fea79d8f80b4f802779525e49d7
Matteo-Grellier <matteogrellier@gmail.com>
2025-06-23 13:59:21 +0200

    feat(husky): Add husky

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,13 @@ import { Admin, EditGuesser, Resource, ShowGuesser } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
+
 import { PostList } from "./posts";
 
 export const App = () => (
 	<Admin dataProvider={dataProvider} layout={Layout}>
 		<Resource name="posts" list={PostList} edit={EditGuesser} />
+
 		<Resource name="users" list={UserList} show={ShowGuesser} />
 	</Admin>
 );
