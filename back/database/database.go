21ff6fbff0fdcb55b7614b1202b71d882aa4390d
Matteo-Grellier <matteogrellier@gmail.com>
2025-07-15 11:28:25 +0200

    feat(cors): setup cors for production

--- back/database/database.go
+++ back/database/database.go
@@ -20,7 +20,7 @@ func NewDB() (*gorm.DB, error) {
 	password := os.Getenv("DB_PASSWORD")
 	name := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, name, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	// db, err := gorm.Open(sqlite.Open("db.db"), &gorm.Config{})
